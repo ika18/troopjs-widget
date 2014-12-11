5ddc5f5dfbf9a16adf40c566757ecb7ca216ddc5
Mikael Karon <mikael@karon.se>
2014-12-11 15:54:56 +0800

    Release 1.0.0-pr.7

--- main.js
+++ main.js
@@ -51,6 +51,7 @@ define('troopjs-widget/config',[
 		"widget": WIDGET
 	}, module.config());
 });
+
 /**
  * @license MIT http://troopjs.mit-license.org/
  */
@@ -263,63 +264,6 @@ define('troopjs-widget/weave',[
 	}
 });
 
-/**
- * @license MIT http://troopjs.mit-license.org/
- */
-define('troopjs-core/component/signal/finalize',[
-	"./stop",
-	"../../config",
-	"when"
-], function (stop, config, when) {
-	var ARRAY_PUSH = Array.prototype.push;
-	var PHASE = "phase";
-	var STOPPED = config.phase.stopped;
-	var FINALIZE = config.phase.finalize;
-	var FINALIZED = config.phase.finalized;
-	var SIG_FINALIZE = config.signal.finalize;
-
-	/**
-	 * @class core.component.signal.finalize
-	 * @implement core.component.signal
-	 * @mixin core.config
-	 * @static
-	 * @alias feature.signal
-	 * @private
-	 */
-
-	/**
-	 * @method constructor
-	 * @inheritdoc
-	 * @localdoc Transitions the component {@link core.component.emitter#property-phase} to `finalized`
-	 */
-	return function finalize() {
-		var me = this;
-		var args = arguments;
-
-		return when(stop.apply(me, args), function (phase) {
-			var _args;
-
-			if (phase === STOPPED) {
-				// Let `me[PHASE]` be `FINALIZE`
-				me[PHASE] = FINALIZE;
-
-				// Let `_args` be `[ SIG_FINALIZE ]`
-				// Push `args` on `_args`
-				ARRAY_PUSH.apply(_args = [ SIG_FINALIZE ], args);
-
-				return me
-					.emit.apply(me, _args)
-					.then(function() {
-						// Let `me[PHASE]` be `FINALIZED`
-						return me[PHASE] = FINALIZED;
-					});
-			}
-			else {
-				return phase;
-			}
-		});
-	}
-});
 /**
  * @license MIT http://troopjs.mit-license.org/
  */
@@ -864,3 +808,6 @@ define('troopjs-widget/plugin',[
 	$FN[WOVEN] = woven;
 });
 
+define(['troopjs-widget/version'], function (version) {
+	return version;
+});
