7e4458f95b1704ec84a24bea3cd4e636afc990da
Mikael Karon <mikael@karon.se>
2014-12-19 16:50:00 +0800

    Release 1.0.0-rc.1

--- buster.js
+++ buster.js
@@ -10,7 +10,16 @@ module.exports["troopjs-widget"] = {
 	],
 
 	"resources": [
-		"**/*.js"
+		"*.js",
+		"test/**/*.js",
+		"bower_components/troopjs-*/**/*.*",
+		"bower_components/mu-*/**/*.*",
+		"bower_components/requirejs/require.js",
+		"bower_components/jquery/dist/jquery.js",
+		"bower_components/when/**/*.js",
+		"bower_components/poly/*.js",
+		"bower_components/poly/lib/*.js",
+		"bower_components/poly/support/*.js"
 	],
 
 	"extensions": [ require("buster-amd") ],
