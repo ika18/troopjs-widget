537b53c9f137d78177b082676dd29411f17912b7
Mikael Karon <mikael@karon.se>
2015-03-06 14:52:25 +0800

    Update `sig/render` to handle `$target` as first argument

--- component.js
+++ component.js
@@ -18,13 +18,11 @@ define([
 	 * @localdoc Adds functionality for working with the loom
 	 */
 
+	var ARRAY_SLICE = Array.prototype.slice;
 	var $ELEMENT = "$element";
 	var SELECTOR_WEAVE = "[" + config.widget.weave + "]";
 	var SELECTOR_WOVEN = "[" + config.widget.woven + "]";
-
-	function widget_weave() {
-		return weave.apply(this[$ELEMENT].find(SELECTOR_WEAVE), arguments);
-	}
+	var FINALIZE = config.phase.finalize;
 
 	/**
 	 * @method constructor
@@ -39,7 +37,9 @@ define([
 		 * @inheritdoc
 		 * @localdoc Calls {@link #method-weave} to ensure newly rendered html is woven
 		 */
-		"sig/render": widget_weave,
+		"sig/render": function ($target) {
+			return weave.apply($target.find(SELECTOR_WEAVE), ARRAY_SLICE.call(arguments, 1));
+		},
 
 		/**
 		 * @handler
@@ -47,7 +47,7 @@ define([
 		 * @localdoc Calls {@link #method-unweave} to ensure this element is unwoven
 		 */
 		"dom/destroy" : function () {
-			if (this.phase !== "finalize") {
+			if (this.phase !== FINALIZE) {
 				unweave.call(this[$ELEMENT]);
 			}
 		},
@@ -56,7 +56,9 @@ define([
 		 * @method
 		 * @inheritdoc widget.weave#constructor
 		 */
-		"weave" : widget_weave,
+		"weave" : function() {
+			return weave.apply(this[$ELEMENT].find(SELECTOR_WEAVE), ARRAY_SLICE.call(arguments));
+		},
 
 		/**
 		 * @inheritdoc widget.unweave#constructor
