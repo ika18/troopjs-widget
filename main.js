c6b5913c4dca7e42c34a66627027d0bee47793a0
Mikael Karon <mikael@karon.se>
2015-03-06 16:53:24 +0800

    Added `mu-*` deps

--- main.js
+++ main.js
@@ -534,13 +534,11 @@ define('troopjs-widget/component',[
 	 * @localdoc Adds functionality for working with the loom
 	 */
 
+	var ARRAY_SLICE = Array.prototype.slice;
 	var $ELEMENT = "$element";
 	var SELECTOR_WEAVE = "[" + config.widget.weave + "]";
 	var SELECTOR_WOVEN = "[" + config.widget.woven + "]";
-
-	function widget_weave() {
-		return weave.apply(this[$ELEMENT].find(SELECTOR_WEAVE), arguments);
-	}
+	var FINALIZE = config.phase.finalize;
 
 	/**
 	 * @method constructor
@@ -555,7 +553,9 @@ define('troopjs-widget/component',[
 		 * @inheritdoc
 		 * @localdoc Calls {@link #method-weave} to ensure newly rendered html is woven
 		 */
-		"sig/render": widget_weave,
+		"sig/render": function ($target) {
+			return weave.apply($target.find(SELECTOR_WEAVE), ARRAY_SLICE.call(arguments, 1));
+		},
 
 		/**
 		 * @handler
@@ -563,7 +563,7 @@ define('troopjs-widget/component',[
 		 * @localdoc Calls {@link #method-unweave} to ensure this element is unwoven
 		 */
 		"dom/destroy" : function () {
-			if (this.phase !== "finalize") {
+			if (this.phase !== FINALIZE) {
 				unweave.call(this[$ELEMENT]);
 			}
 		},
@@ -572,7 +572,9 @@ define('troopjs-widget/component',[
 		 * @method
 		 * @inheritdoc widget.weave#constructor
 		 */
-		"weave" : widget_weave,
+		"weave" : function() {
+			return weave.apply(this[$ELEMENT].find(SELECTOR_WEAVE), ARRAY_SLICE.call(arguments));
+		},
 
 		/**
 		 * @inheritdoc widget.unweave#constructor
