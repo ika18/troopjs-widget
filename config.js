9210a583040d8b7e23f582169ab25c5a422f1fd1
Mikael Karon <mikael@karon.se>
2015-03-26 11:57:45 +0800

    Release 1.0.0-rc.5

--- config.js
+++ config.js
@@ -2,50 +2,50 @@
  * @license MIT http://troopjs.mit-license.org/
  */
 define([
-	"troopjs-dom/config",
-	"module",
-	"mu-merge/main"
+  "troopjs-dom/config",
+  "module",
+  "mu-merge/main"
 ], function (config, module, merge) {
-	"use strict";
+  "use strict";
 
-	/**
-	 * @class widget.config.widget
-	 * @enum
-	 * @private
-	 */
-	var WIDGET = {
-		/**
-		 * Property of the widget where the **weft** resides.
-		 */
-		"$weft" : "$weft",
-		/**
-		 * Attribute name of the element where the **weave** resides.
-		 */
-		"weave" : "data-weave",
-		/**
-		 * Attribute name of the element where the **unweave** resides.
-		 */
-		"unweave" : "data-unweave",
-		/**
-		 * Attribute name of the element where the **woven** resides.
-		 */
-		"woven" : "data-woven"
-	};
+  /**
+   * @class widget.config.widget
+   * @enum
+   * @private
+   */
+  var WIDGET = {
+    /**
+     * Property of the widget where the **weft** resides.
+     */
+    "$weft": "$weft",
+    /**
+     * Attribute name of the element where the **weave** resides.
+     */
+    "weave": "data-weave",
+    /**
+     * Attribute name of the element where the **unweave** resides.
+     */
+    "unweave": "data-unweave",
+    /**
+     * Attribute name of the element where the **woven** resides.
+     */
+    "woven": "data-woven"
+  };
 
-	/**
-	 * Provides configuration for the widget package
-	 * @class widget.config
-	 * @extends dom.config
-	 * @private
-	 * @alias feature.config
-	 */
+  /**
+   * Provides configuration for the widget package
+   * @class widget.config
+   * @extends dom.config
+   * @private
+   * @alias feature.config
+   */
 
-	return merge.call({}, config, {
-		/**
-		 * Widget related configuration
-		 * @cfg {widget.config.widget}
-		 * @protected
-		 */
-		"widget": WIDGET
-	}, module.config());
+  return merge.call({}, config, {
+    /**
+     * Widget related configuration
+     * @cfg {widget.config.widget}
+     * @protected
+     */
+    "widget": WIDGET
+  }, module.config());
 });
